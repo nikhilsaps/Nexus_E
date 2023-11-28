01a12131070b719959b39ce84e2b75ffeda1f8d7
Nikhil Singh <63636149+nikhilsaps@users.noreply.github.com>
2023-11-28 16:32:12 +0530

    Update insight.js

--- insight.js
+++ insight.js
@@ -1,1 +1,17 @@
 console.log("insights data")
+
+
+
+
+const a = document.getElementsByClassName("pi-widgets nautilus-widgets")[0];
+const jsonstring = a.dataset.pageContext;
+
+try {
+  const jsonObject = JSON.parse(jsonstring);
+  console.log(jsonObject.caseSubject.subjectValue);
+  var que = jsonObject.queueName;
+  document.getElementById("annotationText").value =jsonObject.caseId+" "+jsonObject.caseSubject.subjectValue
+} catch (error) {
+  console.error("Error parsing JSON:", error);
+}
+
